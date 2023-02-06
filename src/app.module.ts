6514a9d9fb11db14e59f80f6a24c19f726108b9f
nevmerzhitskaya <lamara@tut.by>
2023-02-06 23:05:45 +0100

    feat: some settings for app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DBModule } from 'utils/DB/DB.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DBModule],
 })
 export class AppModule {}
